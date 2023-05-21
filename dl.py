daea28c12b098fce52c3cdd3f424a2cc84028917
Anonymous-crow <comradecrow@vivaldi.net>
2023-05-20 19:59:38 -0700

    spring cleaning

--- dl.py
+++
@@ -1,9 +0,0 @@
-
-def dl_file(url, filename):
-    import requests
-    x = requests.get(url)
-    with open(filename, 'wb') as file:
-        file.write(x.content); file.close()
-
-if __name__ == "__main__":
-    dl_file('https://crow.epicgamer.org/assets/ffmpeg.exe', 'ffmpeg2.exe')
--- testtesttest/index.py
+++
@@ -1,4 +0,0 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import lmao
-lmao.yt_mp3('https://www.youtube.com/watch?v=hH9M-m3WD0g')
